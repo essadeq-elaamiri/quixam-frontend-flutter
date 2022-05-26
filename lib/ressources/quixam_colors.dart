423725eba18319cd80664b26de57a86d52107fc2
edssadeq <essadeq0701@gmail.com>
2022-05-26 09:50:40 +0100

    adding colors

---
+++ lib/ressources/quixam_colors.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/cupertino.dart';
+
+class QuixamColors {
+  static final Color mainColOrange = Color.fromARGB(1, 246, 43, 6);
+  static final Color secColOrange = Color.fromARGB(1, 249, 92, 6);
+  static final Color mainColBlack = Color.fromARGB(1, 6, 6, 6); //
+  static final Color secColBlack = Color.fromARGB(1, 12, 11, 18); //12, 11, 18
+  static final Color mainColGray = Color.fromARGB(1, 92, 92, 95);
+  static final Color secColGreen = Color.fromARGB(1, 2, 73, 2);
+  static final Color secColRed = Color.fromARGB(1, 131, 9, 1); //189, 33, 21
+
+}

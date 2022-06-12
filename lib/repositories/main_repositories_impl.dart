189704c73fa35c6107bee6faf95bffdeea3181ce
essadeq-elaamiri <essadeq0701@gmail.com>
2022-06-12 04:44:52 +0100

    adding permession in android.app.src.Androidmanifest?xml

--- lib/repositories/main_repositories_impl.dart
+++ lib/repositories/main_repositories_impl.dart
@@ -1,5 +1,11 @@
+import 'package:quixam_frontend_flutter/entities/student.dart';
+import 'package:quixam_frontend_flutter/entities/quiz.dart';
+import 'package:quixam_frontend_flutter/entities/question.dart';
+import 'package:quixam_frontend_flutter/entities/commun_classes.dart';
+import 'package:quixam_frontend_flutter/entities/answer.dart';
 import 'package:quixam_frontend_flutter/entities/teacher.dart';
 import 'package:quixam_frontend_flutter/repositories/main_repository.dart';
+import 'package:http/http.dart' as http;
 
 class MainRepositoryImp implements MainRepository{
   // lets concider that this is teachers login
@@ -22,6 +28,70 @@ class MainRepositoryImp implements MainRepository{
   @override
   Teacher? login(String email, String password) {
     // TODO: implement login
+    throw UnimplementedError();
+  }
+
+  @override
+  void addQuestionsAnswer(Answer answer) {
+    // TODO: implement addQuestionsAnswer
+  }
+
+  @override
+  void addQuizsQuestion(Question question) {
+    // TODO: implement addQuizsQuestion
+  }
+
+  @override
+  void addQuizsStudent(Student student) {
+    // TODO: implement addQuizsStudent
+  }
+
+  @override
+  void addTeachersQuiz(Quiz quiz) {
+    // TODO: implement addTeachersQuiz
+  }
+
+  @override
+  void deleteQuestionsAnswer(Id answerId) {
+    // TODO: implement deleteQuestionsAnswer
+  }
+
+  @override
+  void deleteQuizsQuestion(Id questionId) {
+    // TODO: implement deleteQuizsQuestion
+  }
+
+  @override
+  void deleteQuizsStudent(Id studentId) {
+    // TODO: implement deleteQuizsStudent
+  }
+
+  @override
+  void deleteTeachersQuiz(Id quizId) {
+    // TODO: implement deleteTeachersQuiz
+  }
+
+  @override
+  Future<List<Answer>>? getQuestionAnswers(Id questionId) {
+    // TODO: implement getQuestionAnswers
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<List<Question>>? getQuizsQuestions(Id quizId) {
+    // TODO: implement getQuizsQuestions
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<List<Student>>? getQuizsStudents(Id quizId) {
+    // TODO: implement getQuizsStudents
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<List<Quiz>>? getTeachersQuizzes(Id teacherId) {
+    // TODO: implement getTeachersQuizzes
     throw UnimplementedError();
   }
   
